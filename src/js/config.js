23a39c84fa5e561f86d2ea499d9e7b29cd3f0df9
Arya-Lingayat <aryalingayat2004@gmail.com>
2024-10-20 14:24:32 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2575c27b-d71c-4c8d-94f1-2f52ce11ca20';
+export const MODAL_CLOSE_SEC = 2.5;
